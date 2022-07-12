b3b2b8d43666de46d79a5142e69b1160e325e2a2
tutu5122 <mjperez5122@gmail.com>
2022-07-12 17:54:04 -0400

    Agrgregar Scaffold strain

---
+++ app/views/strains/_strain.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! strain, :id, :name, :origin, :created_at, :updated_at
+json.url strain_url(strain, format: :json)
---
+++ db/migrate/20220712215330_create_strains.rb
@@ -0,0 +1,10 @@
+class CreateStrains < ActiveRecord::Migration[6.1]
+  def change
+    create_table :strains do |t|
+      t.string :name
+      t.string :origin
+
+      t.timestamps
+    end
+  end
+end
